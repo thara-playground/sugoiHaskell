c2e09f13003f52984b8a75ddb4ca55df1634f6bb
TomochikaHara <tomochikahara@gmail.com>
2013-09-16 09:51:11 +0900

    initial commit

---
+++ part5_1.hs
@@ -0,0 +1,62 @@
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x = x : filter p xs
+    | otherwise = filter p xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = filter (<= x) xs
+        larger = filter (> x) xs
+    in  quicksort smallerOrEqual ++ [x] ++ larger
+
+
+
+
+largestDivisible :: Integer
+largestDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
+
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd n  = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
+
